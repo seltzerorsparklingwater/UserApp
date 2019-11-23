628755232355f1057649b4567e8dcee621f68c5d
emily <emily.m.westfall@gmail.com>
2019-11-23 13:14:14 -0500

    update controller, model, create migration for users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,45 @@
 class UsersController < ApplicationController
 
-    def index
-        render plain: "I'm in the index action"
-    end
+  def index
+    render json: User.all
+  end
 
-    def create
-        render json: params
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+      # 422 is the status code for an unprocessable entity.
+      # You can either pass the status code or status symbol.
+      # In other words, you can also return:
+      # render json: user.errors.full_messages, status: 422
     end
+  end
 
-    def show
-        render json: params
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 
 end
