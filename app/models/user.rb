3992f3af1a1985a093f3c8713c0960d5aa8f810c
emily <emily.m.westfall@gmail.com>
2019-11-23 14:31:25 -0500

    add artwork_shares table, add ArtworkShare model, add validations, add associations with user and artwork

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
         foreign_key: :artist_id, 
         class_name: :Artwork,
         dependent: :destroy
+
+    has_many :artwork_shares,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :ArtworkShare,
+        dependent: :destroy
+
+    has_many :shared_artworks,
+        through: :artwork_shares,
+        source: :artwork
 end
