34710eb4fd61b5bfda34a7e2de007d1521e57ec4
emily <emily.m.westfall@gmail.com>
2019-11-23 14:09:04 -0500

    fix association typos

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,6 +5,6 @@ class Artwork < ApplicationRecord
 
     belongs_to :artist,
         primary_key: :id,
-        foreign_key: :artis_id,
+        foreign_key: :artist_id,
         class_name: :User
 end
