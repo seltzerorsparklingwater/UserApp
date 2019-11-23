11f4c590fec3c1e1088de302beae876aa165181b
emily <emily.m.westfall@gmail.com>
2019-11-23 13:55:49 -0500

    add associations between user-artist and artworks

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,4 +3,8 @@ class Artwork < ApplicationRecord
     validates :image_url, uniqueness: true
     validates :title, uniqueness: { scope: :artist_id }
 
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artis_id,
+        class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     validates :username, presence: true
 
+    has_many :artworks,
+        primary_key: :id, #user's id
+        forein_key: :artist_id, 
+        class_name: :Artwork,
+        dependent: :destroy
 end
